c7b5a51cf8f9b4436982a81738a1626453409a49
netdieter <dieter@ew6.org>
2016-09-23 22:23:09 +0200

    Sicherung

---
+++ de.engelhardt.play.app/src/de/engelhardt/play/handler/PartTop.java
@@ -0,0 +1,35 @@
+ 
+package de.engelhardt.play.handler;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.fx.core.di.LocalInstance;
+import org.eclipse.fx.core.log.Logger;
+import org.eclipse.fx.core.log.LoggerCreator;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+
+public class PartTop {
+	private static Logger logger = LoggerCreator.createLogger(PartTop.class);
+	
+	@Inject
+	public PartTop() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(BorderPane parent, @LocalInstance FXMLLoader loader) {
+		loader.setLocation(getClass().getResource("resources.fxml.modul.Test.fxml"));
+		logger.debug("NaviPart");
+		try {
+			parent.setCenter(loader.load());
+		} catch (IOException e) {
+			logger.error(e.getCause().getMessage());
+			e.printStackTrace();
+		}
+	}
+}
