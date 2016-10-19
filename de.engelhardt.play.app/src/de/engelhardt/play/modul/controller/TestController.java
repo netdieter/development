942c492a74633d6cd5eabb7f1e3813d7d90349d9
netdieter <dieter@ew6.org>
2016-10-19 22:07:08 +0200

    .

--- de.engelhardt.play.app/src/de/engelhardt/play/modul/controller/TestController.java
+++ de.engelhardt.play.app/src/de/engelhardt/play/modul/controller/TestController.java
@@ -1,9 +1,12 @@
 package de.engelhardt.play.modul.controller;
 
+import java.util.concurrent.TimeUnit;
+
+import org.controlsfx.dialog.ProgressDialog;
 import org.controlsfx.validation.ValidationSupport;
 import org.controlsfx.validation.Validator;
 
-import de.engelhardt.play.Utils;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,7 +14,6 @@ import javafx.scene.control.TextField;
 
 public class TestController {
 	
-	private static String validStmt = "Dieter";
 	
 	@FXML
 	private Button btnTest;
@@ -22,11 +24,18 @@ public class TestController {
 	ValidationSupport valSup;
 	@FXML
     void onTest(ActionEvent event) {
-		if(valSup.isInvalid()){
-			Utils.showValidationError(valSup);
-		} else {
-			Utils.showMessage("Alles Ok");
-		}
+//		if(valSup.isInvalid()){
+//			Utils.showValidationError(valSup);
+//		} else {
+//			Utils.showMessage("Alles Ok");
+//		}
+		LongRunnigTask lrtservice = new LongRunnigTask(); 
+		ProgressDialog dlg = new ProgressDialog(lrtservice);
+		dlg.setTitle("Haalooooo");
+		dlg.setHeaderText("Bitte warten");
+		dlg.setContentText("Auftrag läuft");
+		new Thread(lrtservice).start();
+		
     }
 
 	@FXML
@@ -34,5 +43,24 @@ public class TestController {
 		valSup = new ValidationSupport();
 		valSup.initInitialDecoration();
 		valSup.registerValidator(txtTest, true , Validator.createEmptyValidator("Test sollte nicht leer sein!"));		
+	}
+	
+	class LongRunnigTask extends Task<Void>{
+
+		@Override
+		protected Void call() throws Exception {
+			for(int i = 0; i < 10; i++){
+				 try {
+					TimeUnit.SECONDS.sleep(1);
+					updateProgress(i, 10);
+					updateMessage("wir sind bei " + i );
+//					this.set
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			return null;
+		}
+		
 	}
 }
